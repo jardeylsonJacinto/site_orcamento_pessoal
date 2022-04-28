5c07d7a935cd22d3e2dae16df4b765e5deb21c14
Jardeylson jacinto <jardeylsong.m@gmail.com>
2022-04-28 16:18:47 -0300

    validando dados

--- js/app.js
+++ js/app.js
@@ -1,49 +1,59 @@
 class Despesa {
 	constructor(ano, mes, dia, tipo, descricao, valor) {
-		this.ano = ano
-		this.mes = mes
-		this.dia = dia
-		this.tipo = tipo
-		this.descricao = descricao
-		this.valor = valor
+		this.ano = ano;
+		this.mes = mes;
+		this.dia = dia;
+		this.tipo = tipo;
+		this.descricao = descricao;
+		this.valor = valor;
 	}
+
+    validarDados(){
+        for (let i in this){
+            if( this[i] == undefined || this[i] == '' || this[i] == null){
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
 
 class Bd {
 
 	constructor() {
-		let id = localStorage.getItem('id')
+		let id = localStorage.getItem('id');
 
 		if(id === null) {
-			localStorage.setItem('id', 0)
+			localStorage.setItem('id', 0);
 		}
 	}
 
 	getProximoId() {
-		let proximoId = localStorage.getItem('id')
-		return parseInt(proximoId) + 1
+		let proximoId = localStorage.getItem('id');
+		return parseInt(proximoId) + 1;
 	}
 
 	gravar(d) {
-		let id = this.getProximoId()
+		let id = this.getProximoId();
 
-		localStorage.setItem(id, JSON.stringify(d))
+		localStorage.setItem(id, JSON.stringify(d));
 
-		localStorage.setItem('id', id)
+		localStorage.setItem('id', id);
 	}
 }
 
-let bd = new Bd()
+let bd = new Bd();
 
 
 function cadastrarDespesa() {
 
-	let ano = document.getElementById('ano')
-	let mes = document.getElementById('mes')
-	let dia = document.getElementById('dia')
-	let tipo = document.getElementById('tipo')
-	let descricao = document.getElementById('descricao')
-	let valor = document.getElementById('valor')
+	let ano = document.getElementById('ano');
+	let mes = document.getElementById('mes');
+	let dia = document.getElementById('dia');
+	let tipo = document.getElementById('tipo');
+	let descricao = document.getElementById('descricao');
+	let valor = document.getElementById('valor');
 
 	let despesa = new Despesa(
 		ano.value, 
@@ -54,5 +64,24 @@ function cadastrarDespesa() {
 		valor.value
 	)
 
-	bd.gravar(despesa)
+    if(despesa.validarDados()){
+        bd.gravar(despesa);
+
+        document.getElementById('modal_titulo').innerHTML = 'Registro inserido com sucesso!';
+        document.getElementById('modal_titulo_div').className = 'modal-header text-success';
+        document.getElementById('modal_conteudo').innerHTML = 'Despesa foi cadastrado com sucesso!';
+        document.getElementById('modal_btn').innerHTML = 'voltar';
+        document.getElementById('modal_btn').className = 'btn btn-success';
+
+        $('#modalRegistraDespesa').modal('show')
+        
+    }else{
+        document.getElementById('modal_titulo').innerHTML = 'Erro na inclusão do registro!';
+        document.getElementById('modal_titulo_div').className = 'modal-header text-danger';
+        document.getElementById('modal_conteudo').innerHTML = 'Erro na gravação! verefique se todos os campos estão preenchidos corretamente!';
+        document.getElementById('modal_btn').innerHTML = 'voltar e corrigir';
+        document.getElementById('modal_btn').className = 'btn btn-danger';
+
+        $('#modalRegistraDespesa').modal('show')
+    }
 }
