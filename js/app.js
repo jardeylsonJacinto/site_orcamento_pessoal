621d591db972b22c3c602727acf486da3a90df1e
Jardeylson jacinto <jardeylsong.m@gmail.com>
2022-04-29 18:48:28 -0300

    filtrando despesas, parte 2

--- js/app.js
+++ js/app.js
@@ -67,7 +67,40 @@ class Bd {
 	}
 
 	pesquisar(despesa){
+		
+		let despesasFiltradas = Array();
+		
+		despesasFiltradas = this.recuperarTodosRegistros();
 
+		//ano
+		if(despesa.ano != ''){
+			despesasFiltradas = despesasFiltradas.filter(d => d.ano == despesa.ano);
+		}
+
+		//mes
+		if(despesa.mes != ''){
+			despesasFiltradas = despesasFiltradas.filter(d => d.mes == despesa.mes);
+		}
+
+		//dia
+		if(despesa.dia != ''){
+			despesasFiltradas = despesasFiltradas.filter(d => d.dia == despesa.dia);
+		}
+
+		//tipo
+		if(despesa.tipo != ''){
+			despesasFiltradas = despesasFiltradas.filter(d => d.tipo == despesa.tipo);
+		}
+
+		//descrição
+		if(despesa.descricao != ''){
+			despesasFiltradas = despesasFiltradas.filter(d => d.descricao == despesa.descricao);
+		}
+
+		//valor
+		if(despesa.valor != ''){
+			despesasFiltradas = despesasFiltradas.filter(d => d.valor == despesa.valor);
+		}
 	}
 }
 
